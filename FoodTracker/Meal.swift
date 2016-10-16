ae00bae8aca4ba48c76bed48585434fe96014090
Peter Georgiev <georgiev2592@gmail.com>
2016-10-15 18:16:42 -0700

    Added delete option Save meal list across app sessions

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class Meal {
-   // MARK: Attributes
+class Meal: NSObject, NSCoding {
    
+   
+   // MARK: Properties
    var name: String
    var photo: UIImage?
    var rating: Int
+   
+   // MARK: Archiving Paths
+   static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+   static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+   
+   // MARK: Types
+   struct PropertyKey {
+      static let nameKey = "name"
+      static let photoKey = "photo"
+      static let ratingKey = "rating"
+   }
    
    // MARK: Initialization
    init?(name: String, photo: UIImage?, rating: Int) {
@@ -21,9 +33,31 @@ class Meal {
       self.photo = photo
       self.rating = rating
       
+      super.init()
+      
       // Initialization should fail if there is no name or if the rating is negative.
       if name.isEmpty || rating < 0 {
          return nil
       }
+   }
+   
+   required convenience init?(coder aDecoder: NSCoder) {
+      let name = aDecoder.decodeObject(forKey: PropertyKey.nameKey) as! String
+      
+      // Because photo is an optional property of Meal, use conditional cast.
+      let photo = aDecoder.decodeObject(forKey: PropertyKey.photoKey) as? UIImage
+      
+      let rating = aDecoder.decodeInteger(forKey: PropertyKey.ratingKey)
+      
+      
+      // Must call designated initializer.
+      self.init(name: name, photo: photo, rating: rating)
+   }
+   
+   // NSCoding
+   func encode(with aCoder: NSCoder) {
+      aCoder.encode(self.name, forKey: PropertyKey.nameKey)
+      aCoder.encode(self.photo, forKey: PropertyKey.photoKey)
+      aCoder.encode(self.rating, forKey: PropertyKey.ratingKey)
    }
 }
--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -16,8 +16,16 @@ class MealTableViewController: UITableViewController {
    override func viewDidLoad() {
       super.viewDidLoad()
       
-      // Load the sample data.
-      loadSampleMeals()
+      // Use the edit button item provided by the table view controller.
+      navigationItem.leftBarButtonItem = editButtonItem
+      
+      // Load any saved meals, otherwise load sample data.
+      if let savedMeals = loadMeals() {
+         meals += savedMeals
+      } else {
+         // Load the sample data.
+         loadSampleMeals()
+      }
    }
    
    func loadSampleMeals() {
@@ -65,26 +73,23 @@ class MealTableViewController: UITableViewController {
       return cell
    }
    
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+      // Return false if you do not want the specified item to be editable.
+      return true
+   }
    
-   /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-    }
-    */
-   
-   /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-    // Delete the row from the data source
-    tableView.deleteRows(at: [indexPath], with: .fade)
-    } else if editingStyle == .insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+      if editingStyle == .delete {
+         // Delete the row from the data source
+         meals.remove(at: indexPath.row)
+         saveMeals()
+         tableView.deleteRows(at: [indexPath], with: .fade)
+      } else if editingStyle == .insert {
+         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+      }
+   }
    
    /*
     // Override to support rearranging the table view.
@@ -114,8 +119,7 @@ class MealTableViewController: UITableViewController {
             let selectedMeal = meals[indexPath.row]
             mealDetailViewController.meal = selectedMeal
          }
-      }
-      else if segue.identifier == "AddItem" {
+      } else if segue.identifier == "AddItem" {
          print("Adding new meal.")
       }
    }
@@ -128,14 +132,29 @@ class MealTableViewController: UITableViewController {
             // Update an existing meal.
             meals[selectedIndexPath.row] = meal
             tableView.reloadRows(at: [selectedIndexPath], with: .none)
-         }
-         else {
+         } else {
             // Add a new meal.
             let newIndexPath = NSIndexPath(row: meals.count, section: 0)
             
             meals.append(meal)
             tableView.insertRows(at: [newIndexPath as IndexPath], with: .bottom)
          }
+      
+         // Save the meals.
+         saveMeals()
       }
+   }
+   
+   // MARK: NSCoding
+   func saveMeals() {
+      let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+   
+      if !isSuccessfulSave {
+         print("Failed to save meals...")
+      }
+   }
+   
+   func loadMeals() -> [Meal]? {
+      return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
    }
 }
